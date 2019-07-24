47c8cc4b4c22e695426694f76c294d58b5a2b789
NguyenDangNguyen <nguyennguyentlu1997@gmail.com>
2019-07-24 10:33:42 +0700

    lap o

---
+++ session3.js
@@ -0,0 +1,26 @@
+// var x
+// let y
+// console.log(x)
+// console.log(y)
+// console.log(`giá trị của x là ${x}`)
+// let a = 10
+// let b = 20
+// console.log(`tong cua ${a} va ${b} la ${a+b}`)
+
+
+
+// var x = 100
+// console.log(x++)
+// console.log(x)
+// var y = 200
+// console.log(++y)
+// console.log(y)
+// var z = 300
+// console.log(--z)
+// var i = 400
+// console.log(i--)
+
+
+let name = prompt("ban ten la gi?")
+let yearofbirth = prompt("ban bao nhieu tuoi?")
+console.log(`Xin chao ${name},ban ${2019-yearofbirth}`);
